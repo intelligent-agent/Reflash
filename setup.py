c4ab779baf40e9209bcfdcaf599d9ea680b0a699
Elias Bakken <elias@iagent.no>
2022-08-07 00:06:32 +0200

    Set version numbers right

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='reflash',
-   version='0.1',
+   version='0.0.3',
    description='Server that can flash Refactor images',
    author='Elias Bakken',
    license="AGPLv3",
