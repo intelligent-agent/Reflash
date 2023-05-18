20e322e5dd2f1bd15b5109ef08e06bcfc2f42d41
Elias Bakken <elias@iagent.no>
2023-05-19 00:35:04 +0200

    Using armbian main

--- armbian/add-scp.sh
+++ armbian/add-scp.sh
@@ -1,4 +1,4 @@
 function fetch_sources_tools__add_scp() { 
   echo "Adding empty scp.bin"
-  touch ../../u-boot/v2023.04/scp.bin
+  touch /armbian/cache/sources/u-boot-worktree/u-boot/v2023.04/scp.bin
 }
